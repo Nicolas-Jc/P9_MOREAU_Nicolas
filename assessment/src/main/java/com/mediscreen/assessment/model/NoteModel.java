886c365af6f77006f7115812ff99b9085a30c51f
Nicolas MOREAU <A151UJ@login.axa>
2023-06-23 13:03:06 +0200

    Sprint 3 : (Fix) - Différentes améliorations (Front, ...)

--- assessment/src/main/java/com/mediscreen/assessment/model/NoteModel.java
+++ assessment/src/main/java/com/mediscreen/assessment/model/NoteModel.java
@@ -17,45 +17,8 @@ public class NoteModel {
         this.doctorNote = doctorNote;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(Integer patientId) {
-        this.patientId = patientId;
-    }
-
-    public String getNoteDate() {
-        return noteDate;
-    }
-
-    public void setNoteDate(String noteDate) {
-        this.noteDate = noteDate;
-    }
-
     public String getDoctorNote() {
         return doctorNote;
     }
 
-    public void setDoctorNote(String doctorNote) {
-        this.doctorNote = doctorNote;
-    }
-
-    @Override
-    public String toString() {
-        return "NoteBean{" +
-                "id='" + id + '\'' +
-                ", patientId=" + patientId +
-                ", noteDate=" + noteDate +
-                ", doctorNote='" + doctorNote + '\'' +
-                '}';
-    }
 }
--- assessment/src/main/java/com/mediscreen/assessment/model/PatientModel.java
+++ assessment/src/main/java/com/mediscreen/assessment/model/PatientModel.java
@@ -30,72 +30,21 @@ public class PatientModel {
         this.phoneNumber = phoneNumber;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public LocalDate getBirthDate() {
         return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
     }
 
     public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "PatientBean{" +
-                "id=" + id +
-                ", lastName='" + lastName + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", birthDate=" + birthDate +
-                ", sex='" + sex + '\'' +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
